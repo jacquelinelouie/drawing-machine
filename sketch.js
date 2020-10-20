d9467ea5def9a588071a6415883177fe082ffc63
jacquelinelouie <jacquelinelouie@users.noreply.github.com>
2020-10-20 11:56:46 -0700

    full screen

--- sketch.js
+++ sketch.js
@@ -1,21 +1,28 @@
 let array = [];
+let backgroundColor = 200;
 
 function setup() {
-  createCanvas(600, 600);
-  background(220);
+  createCanvas(windowWidth, windowHeight);
+  background(backgroundColor);
 
   strokeWeight(5);
   noFill();
 }
 
 function draw() {
-
+  
   if (mouseIsPressed){
+    backgroundColor -= 5;
+    background(backgroundColor);
     // line(mouseX, mouseY, pmouseX, pmouseY);
-    background(0);
     array.push([mouseX, mouseY]);
   }
-
+    beginShape();
+    for(let i = 0; i < array.length; i++){
+    // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    curveVertex(array[i][0], array[i][1])
+  }
+    endShape();
 }
 
 function keyTyped(){
@@ -26,14 +33,12 @@ function keyTyped(){
       } else if (key==='d'){
         //display image
         background(255);
-
-        beginShape();
-        for(let i = 0; i < array.length; i++){
-          // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-          curveVertex(array[i][0], array[i][1])
-      }
-      endShape();
     }
 
       return false;
+}
+
+function mousePressed(){
+  array =[];
+  backgroundColor = 255;
 }
