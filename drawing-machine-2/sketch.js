49065bb7d8bb3fd88489eb1fc08e58990cb18b66
jacquelinelouie <71737257+jacquelinelouie@users.noreply.github.com>
2020-10-22 10:22:12 -0700

    duplicate drawing machine

---
+++ drawing-machine-2/sketch.js
@@ -0,0 +1,44 @@
+let array = [];
+let backgroundColor = 200;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(backgroundColor);
+
+  strokeWeight(5);
+  noFill();
+}
+
+function draw() {
+  
+  if (mouseIsPressed){
+    backgroundColor -= 5;
+    background(backgroundColor);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
+  }
+    beginShape();
+    for(let i = 0; i < array.length; i++){
+    // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    curveVertex(array[i][0], array[i][1])
+  }
+    endShape();
+}
+
+function keyTyped(){
+
+      if (key === 's'){
+        //save this image
+        saveCanvas('fileName', 'png');
+      } else if (key==='d'){
+        //display image
+        background(255);
+    }
+
+      return false;
+}
+
+function mousePressed(){
+  array =[];
+  backgroundColor = 255;
+}
