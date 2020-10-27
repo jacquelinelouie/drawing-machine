7533a63dd82caec2b110a7b8f6ad702e160ea59d
jacquelinelouie <jacquelinelouie@users.noreply.github.com>
2020-10-27 12:40:34 -0700

    tutorials 3.13 and 3.14

---
+++ drawing-machine-3/sketch.js
@@ -0,0 +1,65 @@
+let array = [];
+let backgroundColor = 200;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // background(backgroundColor);
+
+
+  drawGrid();
+  noFill();
+}
+
+function draw() {
+
+  if (mouseIsPressed){
+    backgroundColor -= 5;
+    background(backgroundColor);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
+  }
+    beginShape();
+    for(let i = 0; i < array.length; i++){
+    // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    curveVertex(array[i][0], array[i][1])
+  }
+    endShape();
+}
+
+function keyTyped(){
+
+      if (key === 's'){
+        //save this image
+        saveCanvas('fileName', 'png');
+      } else if (key==='d'){
+        //display image
+        background(255);
+    }
+
+      return false;
+}
+
+function mousePressed(){
+  array =[];
+  backgroundColor = 255;
+}
+
+function drawGrid(){
+  numCells = 20;
+  fillColor = 255;
+  // noStroke();
+  strokeWeight(0);
+
+  for (let i = 0; i <= width; i += width / numCells){
+    for (let j = 0; j<= height; j += height / numCells){
+      if (fillColor === 255){
+        fillColor = 200;
+      } else {
+        fillColor = 255;
+      }
+      fill(fillColor);
+      rect(i, j, width / numCells, height / numCells);
+  }
+}
+strokeWeight(5);
+}
